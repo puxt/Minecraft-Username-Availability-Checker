b940993acaec4e88d66b1c6df3d5bafcabef223f
eremophobia <succducc0@gmail.com>
2022-02-07 09:24:06 +0100

    Changes

--- check.py
+++ check.py
@@ -6,6 +6,7 @@ try:
     import re
     import math
     import concurrent.futures
+    from colored import *
 except ImportError:
     print("Trying to install the required modules! THIS MAY DISPLAY LARGE ERRORS!\nPlease try to run this script again once all of the modules have been successfully installed.\n\n")
     input("press enter to start installing... ")
@@ -15,6 +16,15 @@ except ImportError:
     input("\n\ndone installing modules! please restart the script now. Press enter to continue... ")
     sys.exit()
 
+# Colors 
+redBg = bg('red') 
+greenBg = bg('green') 
+red = fg('red') 
+green = fg('green')
+black = fg('black')
+
+reset = attr('reset')
+
 def check_username(username):
     retry = True
     while retry:
@@ -23,9 +33,9 @@ def check_username(username):
         if not (result or (len(username) < 3) or (len(username) > 16)):
             res = client.get('https://api.mojang.com/users/profiles/minecraft/' + username)
             if res.status_code == 200:
-                print(f'{username} was taken.')
+                print( red + f'{username} was taken.' + reset )
             elif res.status_code == 204:
-                print(f'{username} is available or never used.')
+                print( green + f'{username} is available or never used.' + reset )
                 # from stackoverflow:
                 # concurrent.futures.ThreadPoolExecutor allow only one thread to access the common data structure or location in memory at a time; the threading.Lock() primitive is used to manage this, so race conditions don't occur!
                 available_names.append(username)
@@ -36,29 +46,29 @@ def check_username(username):
                 global rate_limited
                 if not rate_limited:
                     rate_limited = True
-                    print(f'Request is being refused due to IP being rate limited. Waiting {time_to_wait} seconds before reattempting...')
+                    print( redBg + black + f'Request is being refused due to IP being rate limited. Waiting {time_to_wait} seconds before reattempting...' + reset )
                 retry = True
                 time.sleep(time_to_wait)
                 rate_limited = False
                 start_time = time.time()
             else:
                 res.raise_for_status()
-                print(f'Unhandled HTTP status code: {res.status_code}. Exiting...')
+                print( redBg + black + f'Unhandled HTTP status code: {res.status_code}. Exiting...' + reset )
                 sys.exit()
         else:
-            print(f'{username} is an invalid username.')
+            print( red + f'{username} is an invalid username.' + reset )
             invalid_names.append(username)
 
 filepath = sys.argv[1]
 if not os.path.isfile(filepath):
-    print(f'File path {filepath} does not exist. Exiting...')
+    print( redBg + black + f'File path {filepath} does not exist. Exiting...' + reset )
     sys.exit()
 available_names = []
 invalid_names = []
 with open(filepath) as name_list:
     username_list = [line.strip() for line in name_list]
     if not username_list:
-        print(f'{filepath} is empty. Exiting...')
+        print( redBg + black + f'{filepath} is empty. Exiting...' + reset )
         sys.exit()
 regex = re.compile(r'[^a-zA-Z0-9_.]')
 client = requests.Session()
@@ -68,9 +78,9 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
     try:
         executor.map(check_username, username_list)
     except Exception as exc:
-        print(f'There is a problem: {exc}. Exiting...')
+        print( redBg + black + f'There is a problem: {exc}. Exiting...' + reset )
         sys.exit()
 print()
-print(f'Available username(s): {available_names}')
+print( greenBg + black + f'Available username(s): {available_names}' + reset )
 if invalid_names:
-    print(f'Invalid username(s): {invalid_names}')
+    print( redBg + black + f'Invalid username(s): {invalid_names}' + reset )
