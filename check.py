9db666c7087e72305236bf9c6b39a0e9412c5553
eremophobia <succducc0@gmail.com>
2022-02-12 02:49:06 +0100

    Changes

--- check.py
+++ check.py
@@ -1,49 +1,73 @@
 try:
     import sys
-    import os
+    from os import system, name, path
     import time
     import requests
     import re
     import math
     import concurrent.futures
-    from colored import *
+    from pyfiglet import figlet_format
+    from colored import bg, fg, attr
+    # Colors 
+    purpleBg = bg('purple_1a')
+    redBg = bg('red') 
+    greenBg = bg('green') 
+
+    purple = fg('purple_1a')
+    yellow = fg('yellow_1')
+    red = fg('red') 
+    green = fg('green')
+    black = fg('black')
+    
+    reset = attr('reset')
+
 except ImportError:
     print("Trying to install the required modules! THIS MAY DISPLAY LARGE ERRORS!\nPlease try to run this script again once all of the modules have been successfully installed.\n\n")
     input("press enter to start installing... ")
-    os.system("py -m pip install -r requirements.txt")
-    os.system("python -m pip install -r requirements.txt")
-    os.system("python3 -m pip install -r requirements.txt")
+    system("py -m pip install -r requirements.txt")
+    system("python -m pip install -r requirements.txt")
+    system("python3 -m pip install -r requirements.txt")
+    print()
     input("\n\ndone installing modules! please restart the script now. Press enter to continue... ")
     sys.exit()
 
-# Colors 
-redBg = bg('red') 
-greenBg = bg('green') 
-red = fg('red') 
-green = fg('green')
-black = fg('black')
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+clear()
 
-reset = attr('reset')
+def title():
+    print( purple + figlet_format ('M-U-A-C', font = 'poison') + reset )
+    if len(sys.argv) < 2:
+        print( purpleBg + yellow + 'Usage : python3 check.py <file name.txt>' + reset )
+    return ''
+print(title())
 
 def check_username(username):
     retry = True
+    
     while retry:
         retry = False
         result = bool(regex.search(username))
+        
         if not (result or (len(username) < 3) or (len(username) > 16)):
             res = client.get('https://api.mojang.com/users/profiles/minecraft/' + username)
+            
             if res.status_code == 200:
                 print( red + f'{username} was taken.' + reset )
+            
             elif res.status_code == 204:
                 print( green + f'{username} is available or never used.' + reset )
-                # from stackoverflow:
-                # concurrent.futures.ThreadPoolExecutor allow only one thread to access the common data structure or location in memory at a time; the threading.Lock() primitive is used to manage this, so race conditions don't occur!
+                
                 available_names.append(username)
             elif res.status_code == 429:
                 end_time = time.time()
                 global start_time
                 time_to_wait = math.ceil(200 - (end_time - start_time))
                 global rate_limited
+                
                 if not rate_limited:
                     rate_limited = True
                     print( redBg + black + f'Request is being refused due to IP being rate limited. Waiting {time_to_wait} seconds before reattempting...' + reset )
@@ -51,6 +75,7 @@ def check_username(username):
                 time.sleep(time_to_wait)
                 rate_limited = False
                 start_time = time.time()
+            
             else:
                 res.raise_for_status()
                 print( redBg + black + f'Unhandled HTTP status code: {res.status_code}. Exiting...' + reset )
@@ -60,26 +85,34 @@ def check_username(username):
             invalid_names.append(username)
 
 filepath = sys.argv[1]
-if not os.path.isfile(filepath):
+
+if not path.isfile(filepath):
     print( redBg + black + f'File path {filepath} does not exist. Exiting...' + reset )
     sys.exit()
+
 available_names = []
 invalid_names = []
+
 with open(filepath) as name_list:
     username_list = [line.strip() for line in name_list]
+    
     if not username_list:
         print( redBg + black + f'{filepath} is empty. Exiting...' + reset )
         sys.exit()
+
 regex = re.compile(r'[^a-zA-Z0-9_.]')
 client = requests.Session()
 rate_limited = False
 start_time = time.time()
+
 with concurrent.futures.ThreadPoolExecutor() as executor:
     try:
         executor.map(check_username, username_list)
+    
     except Exception as exc:
         print( redBg + black + f'There is a problem: {exc}. Exiting...' + reset )
         sys.exit()
+
 print()
 print( greenBg + black + f'Available username(s): {available_names}' + reset )
 if invalid_names:
